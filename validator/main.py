79ab550fef6fe985d7eea604211dc83f5dd839a1
harrisonchu <harrison@arize.com>
2024-06-27 21:52:03 -0400

    Revert "Juliagomes/jailbreak embeddings cleanup"

--- validator/main.py
+++ validator/main.py
@@ -1,11 +1,15 @@
 import itertools
 import numpy as np
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
-from enum import Enum
-import pandas as pd
+from functools import partial
+from typing import Any, Callable, Dict, List, Optional, Tuple
+from collections import Counter
+import time
+import statistics
 
 import numpy as np
 
+from guardrails import Guard
+from guardrails.llm_providers import PromptCallableException
 from guardrails.utils.docs_utils import get_chunks_from_text
 from guardrails.validator_base import (
     FailResult,
@@ -15,15 +19,10 @@ from guardrails.validator_base import (
     register_validator,
 )
 from llama_index.embeddings.openai import OpenAIEmbedding
+import openai
 
 
-def _embed_function(text: Union[str, List[str]]) -> np.ndarray:
-    """Function used to embed text with OpenAIEmbedding(model="text-embedding-ada-002").
-
-    :param text: Either a string or list of strings that will be embedded.
-
-    :return: Array of embedded input string(s).
-    """
+def _embed_function(text):
     if isinstance(text, str):
         text = [text]
 
@@ -33,14 +32,6 @@ def _embed_function(text: Union[str, List[str]]) -> np.ndarray:
     return np.array(embeddings_out)
 
 
-class EmbeddingChunkStrategy(Enum):
-    """Chunk strategy used in get_chunks_from_text when creating embeddings."""
-    SENTENCE = 0
-    WORD = 1
-    CHAR = 2
-    TOKEN = 3
-
-
 @register_validator(name="arize/jailbreak_embeddings", data_type="string")
 class JailbreakEmbeddings(Validator):
     """Validates that user-generated input does not match dataset of jailbreak
@@ -48,123 +39,137 @@ class JailbreakEmbeddings(Validator):
 
     def __init__(
             self,
-            prompt_sources: List[str] = None,
-            threshold: float = 0.2,
+            threshold: float = 0.8,
+            validation_method: str = "full",
             on_fail: Optional[Callable] = None,
-            embed_function: Optional[Callable] = _embed_function,
-            chunk_strategy: EmbeddingChunkStrategy = EmbeddingChunkStrategy.SENTENCE,
-            chunk_size: int = 100,
-            chunk_overlap: int = 20,
             **kwargs,
     ):
-        """Initialize Arize AI Guard against jailbreak embeddings.
-
-        :param prompt_sources: Examples of jailbreak attempts that we want to Guard against. These examples will
-            be embedded by our embed_function. If the Guard sees a user input message with embeddings that are close
-            to any of the embedded chunks from our jailbreak examples, then the Guard will flag the jailbreak attempt.
-            We recommend adding 10 examples. Fewer examples afffects performance, while additional examples hurts latency.
-            If user does not provide examples, we use our own dataset.
-        :param threshold: Float values between 0.0 and 1.0. Defines the threshold at which a new user input is close
-            enough to an embedded prompt_sources chunk that the Guard flags a jailbreak attempt. The distance is measured
-            as the cosine distance between embeddings.
-        :on_fail: Inherited from Validator.
-        :embed_function: Embedding function used to embed both the prompt_sources and live user input.
-        :chunk_strategy: The strategy to use for chunking when calling Guardrails AI. Strategies include sentence,
-            word, character or token. Details in get_chunks_from_text.
-        :chunk_size: Usage defined by Guardrails AI. The size of each chunk. If the chunk_strategy is "sentences",
-            this is the number of sentences per chunk. If the chunk_strategy is "characters", this is the number of 
-            characters per chunk, and so on. Defaults to 100 through trial-and-error.
-        :chunk_overlap: The number of characters to overlap between chunks. If the chunk_strategy is "sentences", this 
-            is the number of sentences to overlap between chunks. Defaults to 20 through trial-and-error.
-        """
-        super().__init__(on_fail, **kwargs)
-        
+        super().__init__(
+            on_fail, threshold=threshold, validation_method=validation_method, **kwargs
+        )
         self._threshold = float(threshold)
+        if validation_method not in ["full"]:
+            raise ValueError("validation_method must be 'full'.")
+        self._validation_method = "full"
         
-        # Use Arize AI prompts if user does not provide their own.
-        if prompt_sources is None:
+        # users may send in their own source examples, but by default the module will load its own if not specified
+        if kwargs.get("sources") is None:
             import os
             script_dir = os.path.dirname(__file__)  # Get the directory where the script is located
-            # Dataset from public repo associated with arxiv paper https://github.com/verazuo/jailbreak_llms
-            file_path = os.path.join(script_dir, 'jailbreak_prompts_2023_05_07.txt')
-            # We recommend at least 10 examples. Additional examples may adversely affect latency.
-            prompt_sources = pd.read_csv(file_path)["prompt"].tolist()[:10]
-        
-        # Validate user inputs
-        for prompt in prompt_sources:
-            if not prompt or not isinstance(prompt, str):
-                raise ValueError(f"Prompt example: {prompt} is invalid. Must contain valid string data.")
-            
-        self.embed_function = embed_function
+            file_path = os.path.join(script_dir, 'jailbreak_examples.txt')
+            with open(file_path, 'r') as file:
+                self.sources = file.read().splitlines()
+            # few shot only plz
+            self.sources = self.sources[:5]
+        else:
+            self.sources = kwargs.get("sources")
+
+
+        self.embed_function = kwargs.get("embed_function", None)
+        if self.embed_function is None:
+            self.embed_function = _embed_function
+
+        # Check chunking strategy
+        chunk_strategy = kwargs.get("chunk_strategy", "sentence")
+        if chunk_strategy not in ["sentence", "word", "char", "token"]:
+            raise ValueError(
+                "`chunk_strategy` must be one of 'sentence', 'word', 'char', "
+                "or 'token'."
+            )
+        chunk_size = kwargs.get("chunk_size", 100)
+        chunk_overlap = kwargs.get("chunk_overlap", 20)
 
         chunks = [
-            get_chunks_from_text(source, chunk_strategy.name.lower(), chunk_size, chunk_overlap)
-            for source in prompt_sources
+            get_chunks_from_text(source, chunk_strategy, chunk_size, chunk_overlap)
+            for source in self.sources
         ]
         self.chunks = list(itertools.chain.from_iterable(chunks))
 
         # Create embeddings
-        self.source_embeddings = np.array(self.embed_function(self.chunks)).squeeze()  
+        self.source_embeddings = np.array(self.embed_function(self.chunks)).squeeze()
 
-    def validate(self, value: Any, metadata: Dict[str, Any]) -> ValidationResult:
-        """Validation function for the JailbreakEmbeddings validator. If the cosine distance
-        of the user input embeddings is below the user-specified threshold of the closest embedded chunk
-        from the jailbreak examples in prompt sources, then the Guard will return FailResult. If all chunks
-        are sufficiently distant, then the Guard will return PassResult.
+    def get_query_function(self, metadata: Dict[str, Any]) -> Callable:
+        """Get the query function from metadata.
 
-        :param value: This is the 'value' of user input. For the JailbreakEmbeddings Guard, we want
-            to ensure we are validating user input, rather than LLM output, so we need to call
-            the guard with Guard().use(JailbreakEmbeddings, on="prompt")
-
-        :return: PassResult or FailResult.
+        If `query_function` is provided, it will be used. Otherwise, `sources` and
+        `embed_function` will be used to create a default query function.
         """
-        closest_chunk, lowest_distance = self.query_vector_collection(text=value, k=1)[0]
-        metadata["highest_similarity_score"] = lowest_distance
-        metadata["similar_jailbreak_phrase"] = closest_chunk
-        if lowest_distance < self._threshold:
-            # At least one jailbreak embedding chunk was within the cosine distance threshold from the user input embedding
+        query_fn = metadata.get("query_function", None)
+
+        # Check that query_fn or sources are provided
+        if query_fn is not None:
+            return query_fn
+
+        # Check distance metric
+        distance_metric = metadata.get("distance_metric", "cosine")
+        if distance_metric not in ["cosine", "euclidean"]:
+            raise ValueError(
+                "`distance_metric` must be one of 'cosine' or 'euclidean'."
+            )
+
+        # Check embed model
+        embed_function = metadata.get("embed_function", None)
+        return partial(
+            self.query_vector_collection,
+            distance_metric=distance_metric,
+            embed_function=embed_function,
+        )
+
+    def validate_full_text(
+            self, value: Any, query_function: Callable, metadata: Dict[str, Any]
+    ) -> ValidationResult:
+        """Validate the full text in the response."""
+        # Replace LLM response with user input prompt
+        print("THIS IS WHAT THE VALUE IS {}".format(value))
+        most_similar_chunks = query_function(text=value, k=1)
+        if most_similar_chunks is None:
+            metadata["highest_similarity_score"] = 0
+            metadata["similar_jailbreak_phrase"] = ""
+            return PassResult(metadata=metadata)
+
+        most_similar_chunk = most_similar_chunks[0]
+        metadata["highest_similarity_score"] = most_similar_chunk[1]
+        metadata["similar_jailbreak_phrase"] = most_similar_chunk[0]
+        if most_similar_chunk[1] < self._threshold:
             return FailResult(
                 metadata=metadata,
                 error_message=(
-                    f"The following text in your response is similar to our dataset of jailbreaks prompts:\n{value}"
+                        "The following text in your response is similar to our dataset of jailbreaks prompts:\n" + value
                 ),
             )
-        # All chunks exceeded the cosine distance threshold
         return PassResult(metadata=metadata)
+
+    def validate(self, value: Any, metadata: Dict[str, Any]) -> ValidationResult:
+        """Validation function for the ProvenanceEmbeddings validator."""
+        query_function = self.get_query_function(metadata)
+
+        return self.validate_full_text(value, query_function, metadata)
 
     def query_vector_collection(
             self,
             text: str,
             k: int,
+            embed_function: Callable,
+            distance_metric: str = "cosine",
     ) -> List[Tuple[str, float]]:
-        """Embed user input text and compute cosine distances to prompt source embeddings (jailbreak examples).
-
-        :param text: Text string from user message. This will be embedded, then we will calculate the cosine distance
-            to each embedded chunk in our prompt source embeddings. 
-
-        :return: List of tuples containing the closest chunk (string text) and the float distance between that 
-            embedded chunk and the user input embedding.
-        """
 
         # Create embeddings
+        print("THIS IS MY EMBED FUNC {}".format(self.embed_function))
         query_embedding = self.embed_function(text).squeeze()
 
         # Compute distances
-        cos_distances = 1 - (
-                np.dot(self.source_embeddings, query_embedding)
-                / (
-                        np.linalg.norm(self.source_embeddings, axis=1)
-                        * np.linalg.norm(query_embedding)
-                )
-        )
-        
-        # Sort indices from lowest cosine distance to highest distance
-        low_to_high_ind = np.argsort(cos_distances)[:k]
-        
-        # Get top-k closest distances
-        lowest_distances = [cos_distances[j] for j in low_to_high_ind]
+        if distance_metric == "cosine":
+            cos_sim = 1 - (
+                    np.dot(self.source_embeddings, query_embedding)
+                    / (
+                            np.linalg.norm(self.source_embeddings, axis=1)
+                            * np.linalg.norm(query_embedding)
+                    )
+            )
+            top_indices = np.argsort(cos_sim)[:k]
+            top_similarities = [cos_sim[j] for j in top_indices]
+            top_chunks = [self.chunks[j] for j in top_indices]
+        else:
+            raise ValueError("distance_metric must be 'cosine'.")
 
-        # Get top-k closest chunks
-        closest_chunks = [self.chunks[j] for j in low_to_high_ind]
-
-        return list(zip(closest_chunks, lowest_distances))
+        return list(zip(top_chunks, top_similarities))
