1222aa86edbc1b06e863df1afcec4b37b831d65c
Julia Gomes <juliagomes@Julias-MacBook-Pro.local>
2024-07-09 13:21:47 -0700

    Remove constants.py file to fix Colab.

--- validator/__init__.py
+++ validator/__init__.py
@@ -1,4 +1,3 @@
 from .main import ArizeDatasetEmbeddings
-from .constants import ARIZE_DEFAULT_RESPONSE, DEFAULT_FEW_SHOT_TRAIN_PROMPTS
 
-__all__ = ["ArizeDatasetEmbeddings", "ARIZE_DEFAULT_RESPONSE", "DEFAULT_FEW_SHOT_TRAIN_PROMPTS"]
+__all__ = ["ArizeDatasetEmbeddings"]
