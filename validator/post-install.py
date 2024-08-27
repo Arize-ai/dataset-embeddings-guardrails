18fde2d79f8535cb7041b5515deaa9e703958a23
David Tam <david@guardrailsai.com>
2024-08-27 15:47:45 -0700

    fix punkt too

--- validator/post-install.py
+++ validator/post-install.py
@@ -1,8 +1,8 @@
 import nltk
 print("post-install starting...")
-print("Downloading punkt_tab")
+print("Downloading punkt")
 
 
-nltk.download('punkt_tab')
+nltk.download('punkt')
 print("post-install complete!")
 # If you don't have anything to add here you should delete this file.
