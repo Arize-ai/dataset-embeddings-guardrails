b39a32609bb54543148b23ef8d770905b479ab53
Julia Gomes <juliagomes@Julias-MacBook-Pro.local>
2024-10-28 12:31:53 -0700

    Update post install

--- validator/post-install.py
+++ validator/post-install.py
@@ -1,3 +1,7 @@
 print("post-install starting...")
+print("Downloading punkt_tab")
+
+import nltk
+nltk.download('punkt_tab')
 print("post-install complete!")
 # If you don't have anything to add here you should delete this file.
